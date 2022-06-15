2b9a665060e127974782aeabb8afb0394ff76c0c
Marcos Volpi <marcos@marcosvolpi.com>
2022-06-15 12:04:22 -0300

    Add comment test

---
+++ spec/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe CommentsController, type: :controller do
+  let(:params) { {} }
+  let(:params_create) { {id: Article.first.id, comment: {email: "mark3@mark3.com", body: "Hi all!"}} }
+
+  before(:each) do |test|
+    create(:article)
+    #create(:comment)
+  end
+
+  context "POST #create" do
+    it "Add comment" do
+      post :create, params: params_create
+      expect(Comment.where(email: params_create[:comment][:email]).size).to be_equal(1)
+    end
+  end
+
+end
+

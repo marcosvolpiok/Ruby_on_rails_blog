d1cf2b6bb8684496d7afb9310f0b36a3ea9827ac
Marcos Volpi <marcos@marcosvolpi.com>
2022-06-14 21:19:30 -0300

    Add home

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,14 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  get 'blog' => 'articles#index'
+
   scope 'admin' do
     resources :articles, only: %i[create new edit update] do
       collection do
         root to: 'articles#index_admin' 
-        #get 'delete/:id' => 'articles#delete'
         get ':id/delete' => 'articles#delete'
         patch '/' => 'articles#update'
-        
-        
       end
     end
   end
